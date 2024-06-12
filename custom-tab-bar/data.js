7f0c671c58cd3837b20e995ac32b4cafb22f6671
gsons <976955017@qq.com>
2024-06-12 22:37:28 +0800

    first

---
+++ custom-tab-bar/data.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    icon: 'home',
+    text: '首页',
+    url: 'pages/index/index',
+  },
+  {
+    icon: 'lock-off',
+    text: '门禁',
+    url: 'pages/lock/lock',
+  },
+  {
+    icon: 'user',
+    text: '我的',
+    url: 'pages/person/person',
+  },
+];
---
+++ pages/index/index.js
@@ -0,0 +1,84 @@
+const imageCdn = 'https://xcx.pinganbaiyun.cn/p_093_hd/gdfw/images/';
+const swiperList = [
+  `${imageCdn}/ad_003_eletric_001.png`,
+  `${imageCdn}/ad_004_eletric_002.jpg`
+];
+
+
+// pages/index/index.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+      current: 1,
+      autoplay: true,
+      duration: 500,
+      interval: 5000,
+      swiperList,
+
+      visible: true,
+      marquee2: {
+        speed: 60,
+        loop: -1,
+        delay: 0,
+      },
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
+      this.getTabBar().init();
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
+
+    }
+})
